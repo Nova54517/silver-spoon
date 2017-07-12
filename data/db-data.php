4121aef400a956ec58ee81dd139f7152d97fa3ed
Nova54517 <Nova54517@gmail.com>
2017-07-12 15:42:10 -0400

    Settings for DB

---
+++ data/db-data.php
@@ -0,0 +1,8 @@
+// Settings
+
+<?php
+  $username = "silverspoonuser"; // change
+  $password = "password"; // change
+  $server = "localhost" // Change if needed. Though it's recommeneded not to.
+  $dbname = "silver-spoon-db"; // change
+?>
